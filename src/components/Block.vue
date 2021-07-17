b60d620bff105bddd6f04e700c4473eb2cd9b92a
Dusan <dusankubala1996@gmail.com>
2021-07-17 16:22:54 +0200

    Add delay functionality in Block.vue

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="block">
+    <div class="block" v-if="showBlock">
         Click me!
     </div>
 </template>
 
 <script>
 export default {
-    props: [ 'delay' ]
+    props: [ 'delay' ],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        console.log('Component is mounted')
+        setTimeout(() => {
+            this.showBlock = true
+            console.log(this.delay)
+        }, this.delay)
+    }
 }
 </script>
 
